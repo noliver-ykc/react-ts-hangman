2f1610fe873e78e4a11fd0fd3a8dbc7677553288
Nicole Olliver <noliver.ykc@gmail.com>
2023-01-08 21:05:21 +0900

    built hangman drawing, the word to guess dashes and the keyboard buttons which are yet to be designed

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
 import { useState } from "react"
+import words from "./wordList.json"
+
+
+import HangmanDrawing from './components/HangmanDrawing';
+import HangmanWord from "./components/HangmanWord";
+import Keyboard from "./components/Keyboard";
+
+
 function App() {
-  const [wordToGuess, setWordToGuess] = useState("Test")
+  // spits out a random goal word
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random()*words.length)]
+  })
+  // stores user input as an array of strings
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
   return (
-    <h1>Hello world</h1>
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center"
+      }}
+    >
+      <div style={{fontSize: "2rem", textAlign: "center"}}>
+        Loseuwhd
+        Win
+      </div>
+      <HangmanDrawing/>
+      <HangmanWord/>
+      <Keyboard/>
+    </div>
   )
 }
 
