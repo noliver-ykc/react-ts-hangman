64423df0d994146f9918080754633925f99f3c45
Nicole Olliver <noliver.ykc@gmail.com>
2023-01-19 22:15:47 +0900

    word now dynamically changes with each refresh

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ function App() {
         Win
       </div>
       <HangmanDrawing numberOfGuesses={incorrectLetters.length}/>
-      <HangmanWord/>
+      <HangmanWord guessedLetters={guessedLetters} wordToGuess={wordToGuess}/>
       <div style={{alignSelf: "stretch"}}>
         <Keyboard/>
       </div>
